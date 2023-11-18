9092007a6e107aea49772dec96f0e5bfa6a96938
Racingwind <louis.k.hu@gmail.com>
2023-11-18 03:34:24 -0800

    a copy of spinner1, as it's already 'reduced'

---
+++ spinner2.js
@@ -0,0 +1,17 @@
+const timeout = (word, num) => {
+  setTimeout(() => {
+    process.stdout.write(word);
+  }, (num)); // it's in milliseconds
+};
+
+const arr = ["\r|   ", "\r/   ", "\r-   ", "\r\\   "];
+let count = 0;
+
+for (let i = 0; i < 2; i++) {
+  for (const entry of arr) {
+    timeout(entry, 100 + (count * 200));
+    count++;
+  }
+}
+
+timeout("\n", 100 + (count * 200));
